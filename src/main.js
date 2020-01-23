c97b08593ea97d2ebe611338baf1298ac94fd38a
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-01-23 10:33:53 -0500

    added basic UI

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+
+let window;
+
+function createWindow() {
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  window.loadFile('src/index.html');
+  window.webContents.openDevTools();
+  window.on('closed', () => {
+    window = null;
+  });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if (window == null) {
+    createWindow()
+  }
+});
