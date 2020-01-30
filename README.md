c199c064fd4bd08612533ce0559f4f5be821df1c
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-01-29 21:36:50 -0500

    added README.md

---
+++ README.md
@@ -0,0 +1,63 @@
+# YouTube Downloader
+
+## Motivation
+
+This project started from my parents asking me how to download YouTube videos or music from YouTube. 
+The problem is that I didn't want to send my parents to a suspicious online tool filled with ads.
+At first, I made them a Python commandline app to download, but that was too verbose.
+I had the idea to have a try at developing a clean and straightforward UI as a desktop app for them to use. 
+
+---
+
+![GitHub](https://img.shields.io/github/license/gpnn/youtube-downloader-electron?style=flat-square)
+
+![GitHub top language](https://img.shields.io/github/languages/top/gpnn/youtube-downloader-electron?style=flat-square)
+![GitHub language count](https://img.shields.io/github/languages/count/gpnn/youtube-downloader-electron?style=flat-square)
+
+![GitHub commit activity](https://img.shields.io/github/commit-activity/m/gpnn/youtube-downloader-electron?style=flat-square)
+![GitHub last commit](https://img.shields.io/github/last-commit/gpnn/youtube-downloader-electron?style=flat-square)
+
+## Objective
+
+Download YouTube videos as mp3 or mp4, hassle-free. With a an easy-to-use and enjoyable interface.
+
+## Screenshot
+
+Work in progress (WIP) screenshot
+
+![](doc/Screen Shot 2020-01-29 at 9.21.20 PM.png)
+
+## Built with / technologies
+
+* UIkit CSS framework
+* Electron.js
+* Node.js
+
+## Getting started
+
+### For development
+
+* Clone this repo
+* Run `npm install` in the root directory to install dependencies
+* Run `npm start` to start the development server (Electron)
+
+### For general usage
+
+_To be released_ 
+
+### Prerequisites
+
+* Node.js 12+
+
+### Configuration
+
+_Nothing yet_
+
+## Roadmap / Todo
+
+* [ ] Implement back-end logic for downloading
+* [ ] Release alpha
+
+## License
+
+[MIT License](https://choosealicense.com/licenses/mit/#)
--- src/js/downloader.js
+++ src/js/downloader.js
@@ -8,43 +8,43 @@ function downloadYoutube() {
   let re = new RegExp('^((?:https?:)?\\/\\/)?((?:www|m)\\.)?((?:youtube\\.com|youtu.be))(\\/(?:[\\w\\-]+\\?v=|embed\\/|v\\/)?)([\\w\\-]+)(\\S+)?$');
   let url_link = document.getElementById('link-input').value;
   let input_element = document.getElementById('link-input');
-  let updates_element = document.getElementById("updates");
+  let updates_element = document.getElementById('updates');
   console.log(url_link);
 
   let match = re.exec(url_link);
 
   function show_error() {
     let error_alert = document.createElement('div');
-    error_alert.className = "uk-alert-danger";
+    error_alert.className = 'uk-alert-danger';
     let para = document.createElement('p');
-    para.textContent = "Link is not valid";
+    para.textContent = 'Link is not valid';
     let close_button = document.createElement('a');
-    close_button.className = "uk-alert-close";
-    close_button.setAttribute("uk-close", "");
+    close_button.className = 'uk-alert-close';
+    close_button.setAttribute('uk-close', '');
     error_alert.appendChild(close_button);
     error_alert.appendChild(para);
     updates_element.appendChild(error_alert);
 
     setTimeout(() => {
       UIkit.alert(error_alert).close();
-    }, 10000)
+    }, 10000);
   }
 
   function show_success() {
     let success_alert = document.createElement('div');
-    success_alert.className = "uk-alert-success";
+    success_alert.className = 'uk-alert-success';
     let para = document.createElement('p');
-    para.textContent = "Downloading!";
+    para.textContent = 'Downloading!';
     let close_button = document.createElement('a');
-    close_button.className = "uk-alert-close";
-    close_button.setAttribute("uk-close", "");
+    close_button.className = 'uk-alert-close';
+    close_button.setAttribute('uk-close', '');
     success_alert.appendChild(close_button);
     success_alert.appendChild(para);
     updates_element.appendChild(success_alert);
 
     setTimeout(() => {
       UIkit.alert(success_alert).close();
-    }, 10000)
+    }, 10000);
   }
 
   if (!match) {
